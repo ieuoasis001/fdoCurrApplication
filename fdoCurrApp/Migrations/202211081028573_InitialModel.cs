8c4bc198d5d6adafb0ba13a1f7cc4bb2162e1434
fcurukcu <filiz.curukcu@izmirekonomi.edu.tr>
2022-11-08 15:17:35 +0300

    migration eklemesi

---
+++ fdoCurrApp/Migrations/202211081028573_InitialModel.cs
@@ -0,0 +1,32 @@
+﻿namespace fdoCurrApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.lecturerInfo",
+                c => new
+                {
+                    lec_id = c.Int(nullable: false, identity: true),
+                    fdo_id = c.Int(nullable: false),
+                    lec_name = c.String(nullable:false,128),
+                    lec_surname = c.String(nullable: false, 128),
+                    lec_pass = c.String(nullable: false, 32),
+                    auth_key = c.String(nullable: false, 128),
+                    time= c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.lec_id)
+                .Index(t => t.lec_id)
+                .Index(t => t.fdo_id);
+
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
